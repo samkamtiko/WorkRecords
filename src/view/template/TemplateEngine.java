e5a79fc4d25be31eadf52de4a0a2728b65c3d8aa
Ilya Stepykin <i.stepykin@gmail.com>
2016-12-18 00:00:25 +0300

    Finished template implementation

--- src/view/template/TemplateEngine.java
+++ src/view/template/TemplateEngine.java
@@ -1,20 +1,13 @@
 package view.template;
 
 import java.util.HashMap;
-import java.util.NoSuchElementException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class TemplateEngine {
-    private static final String VAR_BEGIN = "<%";
-    private static final String VAR_END = "%>";
-
-    private static final String FOR_BEGIN = "<%!";
-    private static final String FOR_END = "!%>";
-
     private String mTemplate;
     private HashMap<String, String> mVariables;
-    private HashMap<String, Iterable> mCollections;
+    private HashMap<String, Iterable<String>> mCollections;
 
     public TemplateEngine() {
         mVariables = new HashMap<>();
@@ -33,19 +26,28 @@ public class TemplateEngine {
         mCollections.put(name, value);
     }
 
-    public String transform() throws MissingParameterException {
-        String result = mTemplate;
+    private String replaceVariables(String text) throws MissingParameterException {
+        return replaceVariablesWithDict(mVariables, text, false);
+    }
+
+    private String replaceVariablesWithDict(HashMap<String, String> dict, String text, boolean skipUnknown)
+            throws MissingParameterException {
+        String result = text;
         Pattern pattern = Pattern.compile("<%\\h*([\\w]+)\\h*%>");
 
         Matcher matcher = pattern.matcher(result);
         while(matcher.find()) {
             String matched = matcher.group(1);
 
-            if (!mVariables.containsKey(matched)) {
-                throw new MissingParameterException(matched);
+            if (!dict.containsKey(matched)) {
+                if (!skipUnknown) {
+                    throw new MissingParameterException(matched);
+                } else {
+                    continue;
+                }
             }
-            String replacement = mVariables.get(matched);
-            
+            String replacement = dict.get(matched);
+
             // TODO: how to write more efficent?
             result = new StringBuilder(result)
                     .replace(matcher.start(), matcher.end(), replacement)
@@ -55,5 +57,58 @@ public class TemplateEngine {
         }
 
         return result;
+    }
+
+    // Loops are represented in a following way:
+    // <%! for var: collection !%>
+    // <% var %>
+    // <%! endfor !%>
+    public String replaceLoops(String text) throws MissingParameterException {
+        String result = text;
+        // Pattern for matching for expression
+        Pattern begin = Pattern.compile("<%!\\h*for\\h*([\\w]+)\\h*:\\h*([\\w]+)\\h*!%>");
+        // Pattern for matching endfor expression
+        Pattern end = Pattern.compile("<%!\\h*endfor\\h*!%>");
+        Matcher beginMatcher = begin.matcher(result);
+        Matcher endMatcher = end.matcher(result);
+
+        while(beginMatcher.find() && endMatcher.find()) {
+            // For loop block is found
+            // Get indexes of its start and end
+            int blockBegin = beginMatcher.start() + beginMatcher.group().length() + 1;
+            int blockEnd = endMatcher.start();
+
+            // Get name of iterable variable and collection
+            String iteratorName = beginMatcher.group(1);
+            String collectionName = beginMatcher.group(2);
+
+            if (!mCollections.containsKey(collectionName)) {
+                throw new MissingParameterException("collectionName");
+            }
+
+            // Gets the whole block as a string
+            String block = result.substring(blockBegin, blockEnd);
+
+            HashMap<String, String> dict = new HashMap<>();
+            String totalBlock = "";
+
+            for(String var: mCollections.get(collectionName)) {
+                dict.put(iteratorName, var);
+                totalBlock += replaceVariablesWithDict(dict, block, true);
+            }
+
+            // Construct an result.
+            // Note: use start of "for" and end of "endfor" statements as indexes so they
+            // wouldn't appear in final text
+            result = new StringBuilder(result)
+                    .replace(beginMatcher.start(), endMatcher.end(), totalBlock)
+                    .toString();
+        }
+
+        return result;
+    }
+
+    public String generate() throws MissingParameterException {
+        return replaceVariables(replaceLoops(mTemplate));
     }
 }
--- test/controller/template/TemplateEngineTest.java
+++ test/controller/template/TemplateEngineTest.java
@@ -4,6 +4,8 @@ import junit.framework.TestCase;
 import view.template.MissingParameterException;
 import view.template.TemplateEngine;
 
+import java.util.ArrayList;
+
 public class TemplateEngineTest extends TestCase {
 
     public void testTemplateWithoutVariables() {
@@ -11,7 +13,7 @@ public class TemplateEngineTest extends TestCase {
         String templateString = "<h1>Hello world</h1>";
         eng.setTemplate(templateString);
         try {
-            assertEquals(templateString, eng.transform());
+            assertEquals(templateString, eng.generate());
         } catch (MissingParameterException e) {
             assertFalse(true);
         }
@@ -22,7 +24,7 @@ public class TemplateEngineTest extends TestCase {
         eng.setTemplate("<h1>Hello, <% name %></h1>");
         eng.addVariable("name", "username");
         try {
-            assertEquals("<h1>Hello, username</h1>", eng.transform());
+            assertEquals("<h1>Hello, username</h1>", eng.generate());
         } catch (MissingParameterException e) {
             assertFalse(true);
         }
@@ -33,7 +35,7 @@ public class TemplateEngineTest extends TestCase {
         eng.setTemplate("<h1>Hello, <% name %></h1>");
 
         try {
-            eng.transform();
+            eng.generate();
             assertTrue(false);
         } catch(MissingParameterException ex) {
             assertEquals("Parameter value for 'name' is missing", ex.getMessage());
@@ -46,8 +48,43 @@ public class TemplateEngineTest extends TestCase {
         eng.addVariable("word", "hello");
         eng.addVariable("name", "username");
         try {
-            assertEquals("<h1> hello, username</h1>", eng.transform());
+            assertEquals("<h1> hello, username</h1>", eng.generate());
         } catch (MissingParameterException ex) {
+            assertFalse(true);
+        }
+    }
+
+    public void testTemplateSimpleLoop() {
+        TemplateEngine eng = new TemplateEngine();
+        eng.setTemplate("<%! for p : names !%>\n" +
+                            "<h1> <% p %> </h1>\n" +
+                        "<%! endfor !%>");
+        ArrayList<String> names = new ArrayList<>();
+        names.add("username1");
+        names.add("username2");
+        eng.addCollection("names", names);
+        try {
+            assertEquals("<h1> username1 </h1>\n" +
+                         "<h1> username2 </h1>\n", eng.generate());
+        } catch (MissingParameterException e) {
+            assertFalse(true);
+        }
+    }
+
+    public void testTemplateLoopWithVariable() {
+        TemplateEngine eng = new TemplateEngine();
+        eng.setTemplate("<%! for p : names !%>\n" +
+                "<h1> <% p %> <% res %> </h1>\n" +
+                "<%! endfor !%>");
+        ArrayList<String> names = new ArrayList<>();
+        names.add("username1");
+        names.add("username2");
+        eng.addCollection("names", names);
+        eng.addVariable("res", "fine");
+        try {
+            assertEquals("<h1> username1 fine </h1>\n" +
+                    "<h1> username2 fine </h1>\n", eng.generate());
+        } catch (MissingParameterException e) {
             assertFalse(true);
         }
     }
